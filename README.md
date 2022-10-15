06361b231c62620ac4536eafdd53511e179643a6
Thormaz20 <thormazmike@gmail.com>
2022-10-15 13:21:24 +0100

    trying to remember

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # C-PROGRAM
 My journey while learning "C" programming language
+
+making sure i get better at it.
---
+++ scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int aa;
+    printf("enter an int....");
+    scanf("%i", &aa);
+    printf("number entered: %i \n", aa);
+
+    return (0);
+}
